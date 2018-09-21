baf8870f193833ca2fbac9c881e32cfd9952d751
Vivek Rathi <vivekrathi53@gmail.com>
2018-09-21 18:31:26 +0530

    Conflicts solved

--- src/MainPackage/LoadRetailerDetails.java
+++ src/MainPackage/LoadRetailerDetails.java
@@ -1,4 +1,4 @@
-package sample;
+package MainPackage;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/MainPackage/RetalierSWindowController.java
+++ src/MainPackage/RetalierSWindowController.java
@@ -1,4 +1,4 @@
-package sample;
+package MainPackage;
 
 public class RetalierSWindowController {
 }
